c119dfd34aa4beb97d4093b25b3ea23308dca7e9
LeHWd <luis.lehgtr@gmail.com>
2024-12-07 20:11:00 -0600

    actualizando lo mismo

---
+++ Cosas/src/main/java/Entidades/CosasEntidades.java
@@ -0,0 +1,21 @@
+package Entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class CosasEntidades {
+
+    @Id
+    private long idcosa;
+    private String tipo;
+    private String nombre;
+    private String descripcion;
+    private String propietario;
+    private String status;
+}
---
+++ Cosas/src/test/java/com/LeH/Cosas/CosasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LeH.Cosas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
