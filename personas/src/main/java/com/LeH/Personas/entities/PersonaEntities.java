9dfbb9862d12e77bd9b4852260267a024e3f0000
LeHWd <luis.lehgtr@gmail.com>
2024-12-08 23:12:53 -0600

    correciones de Personas

--- personas/src/main/java/com/LeH/Personas/entities/PersonaEntities.java
+++ personas/src/main/java/com/LeH/Personas/entities/PersonaEntities.java
@@ -4,7 +4,7 @@ package com.LeH.personas.entities;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+
 
 import lombok.Getter;
 import lombok.Setter;
--- personas/src/main/java/com/LeH/Personas/repo/PersonaRepo.java
+++ personas/src/main/java/com/LeH/Personas/repo/PersonaRepo.java
@@ -3,12 +3,13 @@ package com.LeH.personas.repo;
 import java.util.List;
 import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
 import com.LeH.personas.entities.PersonaEntities;
 
-public interface PersonaRepo extends MongoRepository<PersonaEntities, Integer> {
+public interface PersonaRepo extends MongoRepository<PersonaEntities, ObjectId> {
     List<PersonaEntities> findAll();
     Optional<PersonaEntities> findByIdpersona(Integer idpersona);
 }
